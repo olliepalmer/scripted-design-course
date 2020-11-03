3f384900df62d3964f0ef6ff6d31dea4fd2dbf63
olliepalmer <github@olliepalmer.com>
2020-11-03 16:19:41 +0100

    Added high contrast mode

---
+++ assets/js/high-contrast.js
@@ -0,0 +1,26 @@
+<script type="text/javascript">
+var hc = false;
+var link = document.createElement("link");
+
+// set properties of link tag
+link.href = "https://sd.olliepalmer.com/assets/css/highcontrast.css";
+link.rel = "stylesheet";
+link.type = "text/css";
+
+// Loaded successfully
+link.onload = function () {
+  console.log("success");
+};
+
+// Loading failed
+link.onerror = function () {
+  console.log("error");
+};
+
+// append link element to html
+function highContrast() {
+  hc = !hc;
+  if (hc) document.body.appendChild(link);
+  if (!hc) document.querySelector('link[href$="https://sd.olliepalmer.com/assets/css/highcontrast.css"]').remove();
+}
+</script>
